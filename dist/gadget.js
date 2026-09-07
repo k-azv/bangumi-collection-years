de5aa07df89784954567213555a7818cf81d5766
kazv <70378740+k-azv@users.noreply.github.com>
2026-09-07 09:27:45 +0800

    Support query strings and fragments on profile pages

--- dist/gadget.js
+++ dist/gadget.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Bangumi 作品年份分布
 // @namespace    https://github.com/k-azv/bangumi-collection-years
-// @version      0.3.9
+// @version      0.3.10
 // @description  按作品发行年份查看动画、书籍、音乐、游戏与三次元收藏
 // @author       k-azv
-// @include      /^https?:\/\/(bgm\.tv|bangumi\.tv|chii\.in)\/user\/[^/?#]+\/?$/
+// @include      /^https?:\/\/(bgm\.tv|bangumi\.tv|chii\.in)\/user\/[^/?#]+\/?(?:[?#].*)?$/
 // @include      /^https?:\/\/(bgm\.tv|bangumi\.tv|chii\.in)\/(anime|book|music|game|real)\/list\/[^/?#]+(?:\/(wish|collect|do|on_hold|dropped))?\/?(?:[?#].*)?$/
 // ==/UserScript==
 (() => {
